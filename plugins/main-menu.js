a4f0f0ee5aca2ac54b3580e6f65c790d3d40dc10
Ricardo272710 <168157929+Ricardo272710@users.noreply.github.com>
2024-07-27 18:15:47 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -4,437 +4,64 @@ import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 
 let tags = {
-  'main': 'Info 📚',
-  'buscador': 'Busquedas 🔎',
-  'fun': 'Juegos 🎮',
-  'jadibot': 'Sub Bots 🤖',
+  'main': 'INFO 📚',
+  'buscador': 'BUSQUEDAS 🔎',
+  'fun': 'JUEGOS 🎮',
+  'jadibot': 'SUB BOTS 🤖',
   'rpg': 'RPG 🌠',
-  'rg': 'Registro 📁',
-  'xp': 'Exp 🏷',
-  'sticker': 'Stickers 🏞',
-  'anime': 'Animes 🍧',
-  'database': 'Database ✨️',
-  'fix': 'Fixmsgespera 💭',
-  'grupo': 'Grupos 👥',
-  'nable': 'On / Off 📴', 
-  'descargas': 'Descargas 📥',
-  'youtube': 'Youtube Play 📥',
-  'tools': 'Herramientas 🔧',
-  'info': 'Información 🐢',
-  'nsfw': 'Nsfw 🔞', 
-  'owner': 'Creador 👑', 
-  'mods': 'Staff Luffy 🍟',
-  'audio': 'Audios 🔉', 
-  'ai': 'Ai 🌹',
-  'transformador': 'Convertidores 🚩',
+  'rg': 'REGISTRO 📁',
+  'xp': 'EXP 🏷',
+  'sticker': 'STICKERS 🏞',
+  'anime': 'ANIMEA 🍧',
+  'database': 'DATABASE ✨️',
+  'fix': 'FIXMSGESPERA 💭',
+  'grupo': 'GRUPOS 👥',
+  'nable': 'ON / OFF 📴', 
+  'descargas': 'DESCARGAS 📥',
+  'youtube': 'YOUTUBE PLAY 📥',
+  'tools': 'HERRAMIENTAS 🔧',
+  'info': 'INFORMACIÓN 🐢',
+  'nsfw': 'NSFW 🔞', 
+  'owner': 'CREADOR 👑', 
+  'mods': 'STAFF LUFFY 🍟',
+  'audio': 'AUDIOS 🔉', 
+  'ai': 'AI 🌹',
+  'transformador': 'CONVERTIDORES 🚩',
 }
 
 const defaultMenu = {
-  before: `*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷*
+  before: `*─ׄ─ׅ─⭒─ׄ─ׄ─⭒─ׅ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׄ─*
 
-“ Hola *%name*, mi nombre es *SpiderBot*, Cómo se encuentra el día de hoy?  ”
+“ Hola *%name* soy *SPIDERBOT*, %greeting ”
 
-*〔🕷️SPIDER MAN BOT 🕸️〕
+╭────═[ *INFO - BOT* ]═─────⋆
+│╭───────────────···
+┴│✯ 💌 *Cliente:* %name
+││✯ ⚡️ *Exp:* %exp
+││✯ 🌟 *Estrellas:* %estrellas
+││✯ 🐢 *Nivel:* %level
+┬│✯ ⚓ *Rango:* %role
+│╰────────────────···
+╰────────═┅═─────────
+%readmore
+╭────═[ *INFO - USER* ]═─────⋆
+│╭───────────────···
+┴│✯ 🍟 *Bot:* Luffy Bot - MD 
+││✯ 🚩 *Modo* Público
+││✯ 📚 *Baileys:* Multi Device
+││✯ 🕒 *Tiempo Activo:* %muptime
+┬│✯ 👤 *Usuarios:* %totalreg
+│╰────────────────···
+╰────────═┅═─────────
+%readmore
+*─ׄ─ׄ─⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ─⭒─ׄ─ׄ─⭒─ׄ─ׅ─*
 
-╭━〔SPIDER MAN 〕━⬣
-┃ACTIVO: 03.06.2024
-┃   CREADORES:
-┃RICARDO—ANTHONY
-╰━━━━━━━━━━━⬣
-╭━〔COMANDOS〕━⬣
-┃.menu | .comandos
-🕷️🕸️🕷️🕸️🕷️🕸️🕷️🕸️🕷️
-┃PERMANENTE | MENSUAL
-╰━━━━━━━━━━━⬣
-
-╭━ VS / REGLAS / FICHA━⬣
-┃🫅🌺 ➺ .Fem4
-┃🫅🌺 ➺ .Fem6
-┃🫅🌺 ➺ .FemInterna4
-┃🫅🌺 ➺ .FemInterna6
-┃🫅🌺 ➺ .vs8fem
-┃🫅🌺 ➺ .vs12fem
-┃🫅🌺 ➺ .scrimfem
-┃🤴 ➺ .Masc4
-┃🤴 ➺ .Masc6
-┃🤴 ➺ .MascInterna4
-┃🤴 ➺ .MascInterna6
-┃👫 ➺ .Mixto4
-┃👫 ➺ .Mixto6
-┃👫 ➺ .MixtoInterna4
-┃👫 ➺ .MixtoInterna6
-┃📏 ➺ .reglasclk
-┃🇬🇬 ➺ .guerra
-┃📐 ➺ .apos
-┃📝 ➺ .Ficha / jugador
-┃👑 ➺ .Vs4
-┃👑 ➺ .Vs6
-┃👑 ➺ .Vs8
-┃👑 ➺ .Vs12
-┃👑 ➺ .Vs16
-┃👑 ➺ .Vs20
-╰━━━━━━━━━━━━⬣
-
-╭━〔 TREND TIK TOK 〕⬣
-┃💃🕺➺ .Trendtiktok
-╰━━━━━━━━━━━⬣
-
-╭━〔 SPAM Y EDITS 〕⬣
-┃🗂️➺ .SpamComu
-╰━━━━━━━━━━━⬣
-
-╭━ MULTI JUEGOS ━⬣
-┃🕷️ ➺ .Abrazo
-┃🕷️ ➺ .Cumple
-┃🕷️ ➺ .Follar
-┃🕷️ ➺ .Huevo
-┃🕷️ ➺ .Formartrio
-┃🕷️ ➺ .Sorteo
-┃🕷️ ➺ .Nalga
-┃🕷️ ➺ .Sega
-┃🕷️ ➺ .math |matemáticas
-┃🕷️ ➺ .lanzar cara | cruz
-┃🕷️ ➺ .ppt piedra : papel : tijera
-┃🕷️ ➺ .tictactoe | ttt sala
-┃🕷️ ➺ .deltictactoe | delttt
-┃🕷️ ➺ .topgays
-┃🕷️ ➺ .topotakus
-┃🕷️ ➺ .toppajer@s
-┃🕷️ ➺ .topput@s
-┃🕷️ ➺ .topintegrantes
-┃🕷️ ➺ .toplagrasa | topgrasa
-┃🕷️ ➺ .toppanafrescos
-┃🕷️ ➺ .topshiposters
-┃🕷️ ➺ .toplindos | toplind@s
-┃🕷️ ➺ .topfamosos/@s
-┃🕷️ ➺ .topparejas
-┃🕷️ ➺ .gay | gay @tag
-┃🕷️ ➺ .gay2 nombre : @tag
-┃🕷️ ➺ .lesbiana nombre : @tag
-┃🕷️ ➺ .manca nombre : @tag
-┃🕷️ ➺ .manco nombre : @tag
-┃🕷️ ➺ .pajero nombre : @tag
-┃🕷️ ➺ .pajera nombre : @tag
-┃🕷️ ➺ .puto nombre : @tag
-┃🕷️ ➺ .puta nombre : @tag
-┃🕷️ ➺ .rata nombre : @tag
-┃🕷️ ➺ .love nombre : @tag
-┃🕷️ ➺ .doxear nombre : @tag
-┃🕷️ ➺ .doxxeame
-┃🕷️ ➺ .pregunta texto
-┃🕷️ ➺ .apostar | slot cantidad
-┃🕷️ ➺ .formarpareja
-┃🕷️ ➺ .dado
-┃🕷️ ➺ .verdad
-┃🕷️ ➺ .reto
-┃🫧➺ .anuel
-┃🫧➺ .ferxxo
-╰━━━━━━━━━━━⬣
-
-╭━━━[ AJUSTES - CHATS ]━━━⬣
-┃⚙️ .on : off bienvenida
-┃⚙️ .on : off avisos
-┃⚙️ .on : off restringir
-┃⚙️ .on : off antillamar
-┃⚙️ .on : off publico
-┃⚙️ .on : off autovisto
-┃⚙️ .on : off temporal
-┃⚙️ .on : off stickers
-┃⚙️ .on : off autosticker
-┃⚙️ .on : off reacciones
-┃⚙️ .on : off audios
-┃⚙️ .on : off modocaliente
-┃⚙️ .on : off antitoxicos
-┃⚙️ .on : off antiver
-┃⚙️ .on : off antieliminar
-┃⚙️ .on : off antinternacional
-┃⚙️ .on : off antienlace
-┃⚙️ .on : off antienlace2
-┃⚙️ .on : off antitiktok
-┃⚙️ .on : off antiyoutube
-┃⚙️ .on : off antitelegram
-┃⚙️ .on : off antifacebook
-┃⚙️ .on : off antinstagram
-┃⚙️ .on : off antitwitter
-┃⚙️ .on : off soloprivados
-┃⚙️ .on : off sologrupos
-╰━━━━━━━━━━━━⬣
-
-╭━〔 GRUPO - RESUMEN 〕━⬣
-┃🔖 ➺ .configuracion
-┃🔖 ➺ .settings
-┃🔖 ➺ .vergrupo
-╰━━━━━━━━━━━⬣
-
-╭━[ DESCARGAS | DOWNLOADS ]━⬣
-┃🕸️ ➺ .imagen | image texto
-┃🕸️ ➺ .pinterest | dlpinterest texto
-┃🕸️ ➺ .wallpaper|wp texto
-┃🕸️ ➺ .play | play2 texto link
-┃🕸️ ➺ .tw |twdl | twitter link
-┃🕸️ ➺ .facebook | fb link
-┃🕸️ ➺ .verig | igstalk usuario(a)
-┃🕸️ ➺ .ighistoria | igstory usuario(a)
-┃🕸️ ➺ .tiktok link
-┃🕸️ ➺ .tiktokimagen | ttimagen link
-┃🕸️ ➺ .tiktokfoto | tiktokphoto usuario(a)
-┃🕸️ ➺ .vertiktok | tiktokstalk usuario(a)
-┃🕸️ ➺ .mediafire | dlmediafire link
-┃🕸️ ➺ .clonarepo | gitclone link
-┃🕸️ ➺ .consejo
-┃🕸️ ➺ .morse codificar texto
-┃🕸️ ➺ .morse decodificar morse
-┃🕸️ ➺ .fraseromantica
-┃🕸️ ➺ .historia
-╰━━━━━━━━━━━⬣
-
-╭━[ CHAT ANONIMO ]━⬣
-┃🧵 ➺ .chatanonimo | anonimochat
-┃🧵 ➺ .anonimoch
-┃🧵 ➺ .start
-┃🧵 ➺ .next
-┃🧵 ➺ .leave
-╰━━━━━━━━━━━⬣
-
-╭━[ CONFIGURACIÓN - GRUPOS ]━⬣
-┃⚪️ ➺ .add numero
-┃⚫️ ➺ .sacar | ban | kick  @tag
-┃🟣 ➺ .grupo abrir : cerrar
-┃🔵 ➺ .group open : close
-┃🟢 ➺ .daradmin | promote @tag
-┃🟡 ➺ .quitar | demote @tag
-┃🟠 ➺ .banchat
-┃🔴 ➺ .unbanchat
-┃🟤 ➺ .banuser @tag
-┃⚪️ ➺ .unbanuser @tag
-┃⚫️ ➺ .admins texto
-┃🟣 ➺ .invocar texto
-┃🔵 ➺ .tagall texto
-┃🟢 ➺ .hidetag texto
-┃🟡 ➺ .infogrupo | infogroup
-┃🟠 ➺ .grupotiempo | grouptime Cantidad
-┃🔴 ➺ .advertencia @tag
-┃🟤 ➺ .deladvertencia @tag
-┃⚪️ ➺ .delwarn @tag
-┃⚫️ ➺ .enlace | link
-┃🟣 ➺ .newnombre | nuevonombre texto
-┃🔵 ➺ .newdesc | descripcion texto
-┃🟢 ➺ .setwelcome | bienvenida texto
-┃🟡 ➺ .setbye | despedida texto
-┃🟠 ➺ .nuevoenlace | resetlink
-┃🔴 ➺ .on
-┃🔘 ➺ .off
-╰━━━━━━━━━━━⬣
-
-╭━━[ CONTENIDO ]━━⬣
-┃🔞➺ .hornymenu
-╰━━━━━━━━━━━⬣
-
-╭━[ CONVERTIDORES ]━⬣
-┃🧸 ➺ .toimg | img | jpg sticker
-┃🧸 ➺ .toanime | jadianime foto
-┃🧸 ➺ .tomp3 |video|nota
-┃🧸 ➺ .tovn |vn|vid|audio
-┃🧸 ➺ .tovideo audio
-┃🧸 ➺ .tourl video, imagen
-┃🧸 ➺ .toenlace  vid|img|au
-┃🧸 ➺ .tts es texto
-╰━━━━━━━━━━━⬣
-
-
-╭━━━[ EFECTOS ]━━⬣
-┃🦠 .simpcard @tag
-┃🦠 .hornycard @tag
-┃🦠 .lolice @tag
-┃🦠 .ytcomment texto
-┃🦠 .itssostupid
-┃🦠 .pixelar
-┃🦠 .blur
-╰━━━━━━━━━━━⬣
-
-╭━[ RANDOM | ANIME ]━⬣
-┃🛣️ .chico
-┃🌁 .cristianoronaldo
-┃🌉 .meme
-┃🌌 .meme2
-┃🌃 .itzy
-┃🏙️ .blackpink
-┃🌇 .kpop blackpink : exo : bts
-┃🎆 .lolivid
-┃🎇 .loli
-┃🌠 .navidad
-┃🌄 .ppcouple
-┃🌅 .neko
-┃🏞️ .waifu
-┃🎑 .akira
-┃🗾 .akiyama
-┃🛣️ .anna
-┃🌁 .asuna
-┃🌉 .ayuzawa
-┃🌃 .boruto
-┃🌌 .chiho
-┃🏙️ .chitoge
-┃🌆 .deidara
-┃🌇 .erza
-┃🎆 .elaina
-┃🌠 .eba
-┃🌄 .emilia
-┃🌅 .hestia
-┃🏞️ .hinata
-┃🎑 .inori
-┃🛣️ .isuzu
-┃🕋 .itachi
-┃🛤️ .itori
-┃🏦 .kaga
-┃🛕 .kagura
-┃⛩️ .kaori
-┃🏤 .keneki
-┃🏣 .kotori
-┃🏪 .kurumi
-┃🏩 .madara
-┃🏥 .mikasa
-┃🏢 .miku
-┃⛰️ .minato
-┃⛺️ .naruto
-┃🌋 .nezuko
-┃🏜️ .sagiri
-┃🏔️ .sasuke
-┃🏝️ .sakura
-┃🏖️ .cosplay
-╰━━━━━━━━━━━⬣
-
-╭━[ MODIFICAR AUDIO]⬣
-┃🚁 .bass
-┃🚆 .blown
-┃🚑 .deep
-┃🚘 .earrape
-┃🚍 .fast
-┃🚜 .fat
-┃🚛 .nightcore
-┃🚒 .reverse
-┃🚌 .robot
-┃🚙 .slow
-┃🚕 .smooth
-┃🚗 .tupai
-╰━━━━━━━━━━━⬣
-
-╭━━[ BÚSQUEDAS🔍]━⬣
-┃🌎 ➺ .animeinfo texto
-┃🌎 ➺ .mangainfo texto
-┃🌎 ➺ .google texto
-┃🌎 ➺ .googlelyrics texto
-┃🌎 ➺ .letra | lirik texto
-┃🌎 ➺ .ytsearch | yts texto
-┃🌎 ➺ .wiki | wikipedia texto
-╰━━━━━━━━━━━⬣
-
-╭━[ HERRAMIENTAS ]━⬣
-┃🛠️ .afk motivo
-┃🛠️ .acortar url
-┃🛠️ .calc operacion math
-┃🛠️ .del 
-┃🛠️ .qrcode texto
-┃🛠️ .readmore texto1|texto2
-┃🛠️ .spamwa #/texto/10
-┃🛠️ .styletext texto
-┃🛠️ .traducir texto
-┃🛠️ .morse codificar texto
-┃🛠️ .morse decodificar
-┃🛠️ .encuesta | poll Motivo
-┃🛠️ .horario
-╰━━━━━━━━━━━⬣
-
-╭━[ FUNCIÓN RPG ]━⬣
-┃🥧 ➺ .pase premium
-┃🍿 ➺ .pass premium
-┃🥛 ➺ .listapremium
-┃🥫➺ .transfer tipo cantidad 
-┃🍛 ➺ .dar tipo cantidad 
-┃🦪 ➺ .enviar tipo cantidad 
-┃🍦 ➺ .balance
-┃🍘 ➺ .cartera | wallet
-┃🌰 ➺ .experiencia | exp
-┃☕️ ➺ .top | lb | leaderboard
-┃🍼 ➺ .rol | rango
-┃🥮 ➺ .inventario | inventory
-┃🍤 ➺ .aventura | adventure
-┃🍚 ➺ .caza | cazar | hunt
-┃🍩 ➺ .pescar | fishing
-┃🍫 ➺ .animales
-┃🍭 ➺ .alimentos
-┃🥟 ➺ .curar | heal
-┃🥓 ➺ .sell
-┃🍝 ➺ .verificar | registrar
-┃🥩 ➺ .perfil | profile
-┃🥗 ➺ .myns
-┃🍔 ➺ .unreg # de serie
-┃🌭 ➺ .minardiamantes 
-┃🧇 ➺ .minarcoins
-┃🥖 ➺ .minarexp
-┃🧀 ➺ .minar : minar2
-┃🥚 ➺ .minar3
-┃🧁 ➺ .reclamar | regalo 
-┃🍧 ➺ .cadahora | hourly
-┃🍰 ➺ .cadasemana  
-┃🥪 ➺ .cadames | mes 
-┃🍱 ➺ .cofre | abrircofre 
-┃🍖 ➺ .trabajar | work
-╰━━━━━━━━━━━⬣
-
-╭━[ TOP EN OPTIMUS ]━⬣
-┃🏆➺ .top
-╰━━━━━━━━━━━⬣
-
-╭━[ STICKERS ]━⬣
-┃🌮 .sticker | s img/video
-┃🍟 .sticker | s url tipo jpg
-┃🥨 .emojimix 😺+😆
-┃🧊 .dado
-╰━━━━━━━━━━━⬣
-
-╭━[ MODIFICAR STICKERS ]━⬣
-┃🧃 .wm packname|author
-┃🧃 .wm texto1|texto2
-╰━━━━━━━━━━━⬣
-
-╭━[ STICKERS DINÁMICOS ]━⬣
-┃🍥 .palmaditas | pat @tag
-┃🍥 .bofetada | slap @tag
-┃🍥 .golpear @tag
-┃🍥 .besar | kiss @tag
-┃🍥 .alimentar | food @tag
-╰━━━━━━━━━━━⬣
-
-╭━[ MENU PARA PROPIETARIO/A ]━⬣
-┃💎➺ .join enlace
-┃💎➺ .unete enlace
-┃💎➺ .dardiamantes cantidad
-┃💎➺ .darxp cantidad
-┃💎➺ .darcoins cantidad
-┃💎➺ .addprem | userpremium @tag cantidad
-┃💎➺ .addprem2 | userpremium2 @tag cantidad
-┃💎➺ .addprem3 | userpremium3 @tag cantidad
-┃💎➺ .addprem4 | userpremium4 @tag cantidad
-┃💎➺ .idioma | language
-┃💎➺ .cajafuerte
-┃💎➺ .comunicar | broadcastall | bc texto
-┃💎➺ .broadcastchats | bcc texto
-┃💎➺ .comunicarpv texto
-┃💎➺ .broadcastgc texto
-┃💎➺ .comunicargrupos texto
-┃💎➺ .borrartmp | cleartmp
-┃💎➺ .delexp @tag
-┃💎➺ .delcoins @tag
-┃💎➺ .deldiamantes @tag
-┃💎➺ .reiniciar | restart
-┃💎➺ .ctualizar | update
-┃💎➺ .addprem | +prem @tag
-┃💎➺ .delprem | -prem @tag
-┃💎➺ .listapremium | listprem
-┃💎➺ .añadirdiamantes @tag cantidad
-┃💎➺ .añadirxp @tag cantidad
-┃💎➺ .añadircoins @tag cantidad
-╰━━━━━━━━━━⬣*,
+\t*L I S T A  -  D E  -  C O M A N D O S* 
+`.trimStart(),
+      header: '╭───═[ %category ]═────⋆\n│╭───────────────···',
+  body: '││%cmd\n',
+  footer: '│╰────────────────···\n╰────────═┅═─────────\n',
+  after: `• ${textbot}`,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
   try {
@@ -545,11 +172,11 @@ const pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegr
   const gif = await response.buffer()
  // const img = imagen1
 
-await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲𝗹 𝗕𝗼𝘁...𓏲੭*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '🍟 ¡Super Bot De WhatsApp!', sourceUrl: redes, thumbnail: icons }}})
+// await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲𝗹 𝗕𝗼𝘁...𓏲੭*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: packname, body: '🍟 ¡Super Bot De WhatsApp!', sourceUrl: redes, thumbnail: icons }}})
 
 // await conn.reply(m.chat, '*ꪹ͜𓂃͡𝗖𝗮𝗿𝗴𝗮𝗻𝗱𝗼 𝗘𝗹 𝗠𝗲𝗻𝘂 𝗗𝗲𝗹 𝗕𝗼𝘁...𓏲੭*', m, rcanal)
 
-m.react(emoji2) 
+await m.react('⭐️') 
 
 await conn.sendFile(m.chat, imagen1, 'luffy.jpg', text.trim(), fkontak, null, rcanal)
 
